b8c606cefe33248f103500a514709a2df638c6d8
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2025-09-02 14:58:40 +0300

    Update cinmana.kt

--- cinmana.kt
+++ cinmana.kt
@@ -1,3 +1,1 @@
-Incorrect package="com.example" found in source AndroidManifest.xml: C:\Users\USER\StudioProjects\cloudstream-extensions-phishero\Tamilblasters\src\main\AndroidManifest.xml.
-Setting the namespace via the package attribute in the source AndroidManifest.xml is no longer supported.
-Recommendation: remove package="com.example" from the source AndroidManifest.xml: C:\Users\USER\StudioProjects\cloudstream-extensions-phishero\Tamilblasters\src\main\AndroidManifest.xml.
+<manifest xmlns:android="http://schemas.android.com/apk/res/android">
