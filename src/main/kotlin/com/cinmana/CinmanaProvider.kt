d08581ecb6a0a7ff99cc45cd2dd321e0a2e4e5e6
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2025-09-04 17:28:36 +0300

    Update CinmanaProvider.kt

--- src/main/kotlin/com/cinmana/CinmanaProvider.kt
+++ src/main/kotlin/com/cinmana/CinmanaProvider.kt
@@ -1,15 +1,12 @@
-package com.Phisher98
+package com.example
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.extractors.Vidguardto2
-import com.lagradost.cloudstream3.extractors.Vidmoly
-import com.lagradost.cloudstream3.extractors.Voe
-import com.lagradost.cloudstream3.extractors.Wishonly
 
 @CloudstreamPlugin
-class FourKHDHubProvider: BasePlugin() {
+class ShabakatyCinemanaPlugin : BasePlugin() {
     override fun load() {
-        registerMainAPI(FourKHDHub())
+        // هنا نسجل المزود الخاص بنا
+        registerMainAPI(ShabakatyCinemanaProvider())
     }
 }
