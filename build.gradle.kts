cc9fcf8f7402b5ee1c67008c14f0b97a56b4534b
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2025-09-06 17:15:52 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,7 @@
 plugins {
-    id("com.android.library")
-    kotlin("android")
-}
-
-android {
-    namespace = "com.cinemana"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
+    kotlin("jvm") version "1.8.0"
 }
 
 dependencies {
-    // لازم يربط بالمكتبة الرئيسية
-    implementation(project(":app"))
-
-    // مكتبات أساسية
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.jsoup:jsoup:1.15.4")
+    implementation(project(":app")) // إذا كان يحتاج للوصول للكود الأساسي
 }
