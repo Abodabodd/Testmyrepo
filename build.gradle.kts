a8f1b8920b415cca417f4fcd53975c92900732f2
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2025-09-06 17:32:18 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,25 @@
-package com.cinemana
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+android {
+    namespace = "com.cinemana"
+    compileSdk = 34
 
-@CloudstreamPlugin
-class CinemanaProviderPlugin : BasePlugin() {
-    override fun load() {
-        registerMainAPI(CinemanaProvider())
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("com.lagradost:cloudstream3-core:1.0.0") // ضع نسخة Core الصحيحة
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
