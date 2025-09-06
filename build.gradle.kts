ebb18785dce0633c1e3c0bf6fbaa3fb1f76f9488
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2025-09-06 17:44:23 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,6 @@
-rootProject.name = "CinemanaExtension"
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.cinemana">
+
+    <uses-sdk android:minSdkVersion="21" android:targetSdkVersion="34" />
+
+</manifest>
