c9e86bf8304bfd1caf12cad411221d4e4d866000
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2025-09-08 14:12:25 +0300

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="com.cinemana">
-
-    <uses-sdk android:minSdkVersion="21" android:targetSdkVersion="34" />
-
-</manifest>
