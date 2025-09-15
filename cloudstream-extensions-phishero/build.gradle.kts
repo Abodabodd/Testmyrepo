040470b258b4542e5947cc5ffca2e50f7ddde3a5
Abodabodd <122629521+Abodabodd@users.noreply.github.com>
2025-09-15 20:32:33 +0300

    Delete cloudstream-extensions-phishero directory

--- cloudstream-extensions-phishero/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// In the ROOT build.gradle.kts (OUTSIDE the Arabseed folder)
-
-plugins {
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-}
